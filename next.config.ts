15d0a6ad2e2327fdeb48850aecda73cc1c279390
Souzalift <souzalift@gmail.com>
2025-02-28 10:29:47 -0300

    chore: update configuration files and dependencies for improved formatting and linting

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { hostname } from "os";
+import { hostname } from 'os';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+    remotePatterns: [{ hostname: 'u9a6wmr3as.ufs.sh' }],
   },
 };
 
