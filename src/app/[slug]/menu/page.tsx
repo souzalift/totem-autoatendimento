4897f645f54207c21a63f721dcd18fa0f1965971
Souzalift <souzalift@gmail.com>
2025-02-28 09:45:54 -0300

    feat: add restaurant categories component and scroll area

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,16 +1,16 @@
-import { ChevronLeft, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
-import { notFound } from "next/navigation";
+import { notFound } from 'next/navigation';
 
-import { Button } from "@/components/ui/button";
-import { db } from "@/lib/prisma";
+import { db } from '@/lib/prisma';
+
+import RestaurantCategories from './components/categories';
+import RestaurantHeader from './components/header';
 interface RestaurantMenuPageProps {
   params: Promise<{ slug: string }>;
   searchParams: Promise<{ consumptionMethod: string }>;
 }
 
 const isConsumptionMethodValid = (consumptionMethod: string) => {
-  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+  return ['DINE_IN', 'TAKEAWAY'].includes(consumptionMethod.toUpperCase());
 };
 
 const RestaurantMenuPage = async ({
@@ -24,26 +24,19 @@ const RestaurantMenuPage = async ({
   }
   const restaurant = await db.restaurant.findUnique({
     where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
+      },
+    },
   });
   if (!restaurant) {
     return notFound();
   }
   return (
     <div>
-      <div className="relative h-[250px] w-full">
-        <Button variant="secondary" size="icon" className="absolute top-4 left-4 z-50 rounded-full">
-        <ChevronLeft />
-        </Button> 
-      <Image
-      src={restaurant?.coverImageUrl}
-      alt={restaurant?.name}
-      fill 
-      className="object-cover"
-      />
-        <Button variant="secondary" size="icon" className="absolute top-4 right-4 z-50 rounded-full">
-        <ScrollTextIcon />
-        </Button> 
-      </div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
